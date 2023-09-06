f6d556fa208898fef3f5478c8abab480b7e34842
Tienz <94908235+tien123g@users.noreply.github.com>
2023-09-06 17:58:14 +0000

    submit

---
+++ Test/test.js
@@ -0,0 +1,24 @@
+const FCC = "freeCodeCamp"; // Change this line
+var fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+const sum = 10 + 10;
+console.log(sum);
+var sumn = 20 + 10;
+console.log(sumn);
+var a = 0; b = 10;
+var a = ++b;
+//a sẽ là 11; b là 11
+console.log(a,b);
+var myDemical=5.7;
+console.log(myDemical);
+var sodu=5%3;
+console.log(sodu);
+var myName = "tien";
+var myaddr =null;
+const name_default = "KHONG TEN";
+const addr_default = "KHONG DIA CHI";
+myName ??= name_default;
+myaddr ??= addr_default;
+console.log(myName);
+console.log(myaddr);
